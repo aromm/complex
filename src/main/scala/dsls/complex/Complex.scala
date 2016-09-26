c7f5eb59e37a018acc19158ea47d507b0bce736c
Ben Wiedermann <benw@cs.hmc.edu>
2016-09-26 11:44:57 -0700

    (infix) operators

--- src/main/scala/dsls/complex/Complex.scala
+++ src/main/scala/dsls/complex/Complex.scala
@@ -1,7 +1,19 @@
 package dsls.complex
 
 case class Complex(real: Double = 0, imaginary: Double = 0) {
-    override def toString = 
+  def +(other: Complex) = {
+    val newReal = this.real + other.real
+    val newImaginary = this.imaginary + other.imaginary
+    Complex(newReal, newImaginary)
+  }
+    
+  def *(other: Complex) = {
+    val newReal = (this.real * other.real) - (this.imaginary * other.imaginary)
+    val newImaginary = (this.real * other.imaginary) + (this.imaginary * other.real)
+    Complex(newReal, newImaginary)
+  }
+
+  override def toString = 
     if (imaginary == 0) 
       s"${real}" 
     else
