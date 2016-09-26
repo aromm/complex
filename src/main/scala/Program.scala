3851449878f650752a77578350e37a2b3f789511
Ben Wiedermann <benw@cs.hmc.edu>
2016-09-26 11:47:51 -0700

    literal extension

--- src/main/scala/Program.scala
+++ src/main/scala/Program.scala
@@ -2,6 +2,6 @@ import dsls.complex.Complex
 
 object Program extends App {
   val c1 = Complex(1, 2)
-  val c2 = Complex(1, 2)
-  println(-c1)
+  val c2 = -1
+  println(c1 * c2)
 }
--- src/main/scala/dsls/complex/Complex.scala
+++ src/main/scala/dsls/complex/Complex.scala
@@ -1,5 +1,8 @@
 package dsls.complex
 
+// we should use this line to enable implicit conversions
+import scala.language.implicitConversions
+
 case class Complex(real: Double = 0, imaginary: Double = 0) {
   def +(other: Complex) = {
     val newReal = this.real + other.real
@@ -20,4 +23,9 @@ case class Complex(real: Double = 0, imaginary: Double = 0) {
       s"${real}" 
     else
      s"${real} + ${imaginary}i"
+}
+
+// This is called a "companion object"
+object Complex {
+  implicit def int2complex(i: Int): Complex = Complex(i)
 }
