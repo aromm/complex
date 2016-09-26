085891c946323ffe81a3cd3b822dba3e9110733c
Ben Wiedermann <benw@cs.hmc.edu>
2016-09-26 11:43:08 -0700

    toString

--- src/main/scala/dsls/complex/Complex.scala
+++ src/main/scala/dsls/complex/Complex.scala
@@ -1,3 +1,9 @@
 package dsls.complex
 
-case class Complex(real: Double = 0, imaginary: Double = 0)
+case class Complex(real: Double = 0, imaginary: Double = 0) {
+    override def toString = 
+    if (imaginary == 0) 
+      s"${real}" 
+    else
+     s"${real} + ${imaginary}i"
+}
